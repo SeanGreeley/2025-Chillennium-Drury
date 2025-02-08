ce272873530ab2abcd81c688b3907efa3729dcb7
Justin Benjamin Kroh <122898766+KosmicKroh@users.noreply.github.com>
2025-02-08 13:39:07 -0600

    Added Video Call Scene

---
+++ scripts/video_call.gd
@@ -0,0 +1,13 @@
+extends Control
+
+var rawRatio = 0.0
+
+func _ready():
+	$Speech.visible_ratio = 0
+	rawRatio = 0
+	$Stephen.play()
+
+func _process(delta):
+	if $Speech.visible_ratio < 1:
+		rawRatio += 20.0 * delta/(len($Speech.text)+1.0)
+		$Speech.visible_ratio = floor(rawRatio*40)/40
