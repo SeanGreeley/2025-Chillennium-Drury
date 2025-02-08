a48b61b54da4c566eb7d327ad5658f4dfe97e15b
SeanGreeley <robloxiansean55@gmail.com>
2025-02-08 14:09:21 -0600

    Updated Global structure, and email handling

--- scripts/EmailController.gd
+++ scripts/EmailController.gd
@@ -1,17 +1,21 @@
 extends Control
 
-var Opener = {"name"="Stephen Careers", "subject"="Welcome Newbie", "text"="Hello World", "links"=null}
+var Opener = {"Welcome Newbie":{"name":"Stephen Careers", "text":"Hello World", "links":null}}
 var mail = Global.emails
+var mailSize = Global.emailSize
+var random_key = mail.keys()[randi() % mailSize]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$ItemList.add_item(Opener["subject"])
-	$ItemList.add_item(mail[0]["subject"])
-	$ItemList.add_item(mail[1]["subject"])
-	$ItemList.add_item("WHO DOESN'T LOVE MONEY?")
+	#$ItemList.add_item(Opener.("Welcome Newbie"))
+	$ItemList.add_item(random_key)
+	#$ItemList.add_item(mail)
+	#$ItemList.add_item("WHO DOESN'T LOVE MONEY?")
 	
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _openMail():
+	$VBoxContainer/Subject.text = mail[$ItemList.get_item_text()]["name"]
