f6355d461fd54a63e8231fac354d6a525a76fe79
Justin Benjamin Kroh <122898766+KosmicKroh@users.noreply.github.com>
2025-02-08 18:34:47 -0600

    Polished Video Call Scene

--- scripts/video_call.gd
+++ scripts/video_call.gd
@@ -5,9 +5,15 @@ var rawRatio = 0.0
 func _ready():
 	$Speech.visible_ratio = 0
 	rawRatio = 0
-	$Stephen.play()
+	$Stephen.play("loading")
+	$CallSound.play()
+	await get_tree().create_timer(3.0).timeout
+	$Stephen.play("talking")
+	$TalkSound.play()
 
 func _process(delta):
-	if $Speech.visible_ratio < 1:
+	if $Speech.visible_ratio < 1 and $Stephen.animation == "talking":
 		rawRatio += 20.0 * delta/(len($Speech.text)+1.0)
 		$Speech.visible_ratio = floor(rawRatio*50)/50
+	elif $Speech.visible_ratio >= 1:
+		$TalkSound.stop()
