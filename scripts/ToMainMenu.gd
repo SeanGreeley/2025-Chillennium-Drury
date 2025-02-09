c9d5ee02a44ab603065f861a572c6ecf0a8518d5
Justin Benjamin Kroh <122898766+KosmicKroh@users.noreply.github.com>
2025-02-09 11:29:41 -0600

    Stuffs

--- scripts/ToMainMenu.gd
+++ scripts/ToMainMenu.gd
@@ -5,4 +5,6 @@ func _ready() -> void:
 	pressed.connect(self.button_pressed)
 	
 func button_pressed() -> void:
+	$ClickSound.play()
+	await get_tree().create_timer(0.35).timeout
 	get_tree().change_scene_to_file("res://scenes/GameTitle.tscn")
