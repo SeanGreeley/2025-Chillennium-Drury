2658e45ee81f46f664d6ad24a45f71fdb5d56327
SeanGreeley <robloxiansean55@gmail.com>
2025-02-08 21:51:13 -0600

    Warning Pop-Up WIP

--- scripts/EmailController.gd
+++ scripts/EmailController.gd
@@ -2,18 +2,19 @@ extends Control
 var mail = Global.emails
 var mailSize = Global.emailSize - 1
 var random_key = mail.keys()[randi() % mailSize + 1]
-var employees = [
-	"bert", 
-	"katherine", 
-	"molly", 
-	"winston"]
+var employees = {
+	"Bert":"res://assets/companyAvatars/bert.png",
+	"Katherine":"res://assets/companyAvatars/katherine.png", 
+	"Molly":"res://assets/companyAvatars/molly.png", 
+	"Winston":"res://assets/companyAvatars/winston.png"
+	}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$ItemList.connect("item_clicked", _openMail)
 	for empNum in range(employees.size()):
-		var currEmp = employees[empNum]
-		$EmployeeList.add_item(currEmp.capitalize())#, load("res://assets/companyAvatars/.png")) % currEmp
+		var currEmp = (employees.keys()[empNum])
+		$EmployeeList.add_item(currEmp, load(employees[currEmp]))
 	$ItemList.add_item(mail.keys()[0])
 	$ItemList.add_item(random_key)
 	
